a1da764f2b1626a237f5785fbf424eff18bd63d0
vista <vista.lyna@gmail.com>
2025-06-08 21:08:29 +0800

    perubahan tipe file

--- web/client/src/scripts/index.js
+++ web/client/src/scripts/index.js
@@ -1,7 +1,6 @@
-
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './pages/App.jsx';
+import App from './pages/App.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
